f407a339600d0a3c63c62883998b3f021315ce7e
Leia <leiahopf@googlemail.com>
2020-06-01 21:53:07 +0100

    Feat: Basics in JS.

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,8 +1,41 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButtons = document.querySelectorAll('[data-equals]');
-const deleteButtons = document.querySelectorAll('[data-delete]');
-const allClearButtons = document.querySelectorAll('[data-all-clear]');
-const previousOperand = document.querySelectorAll('[data-previous-operand]');
-const currentOperand = document.querySelectorAll('[data-current-operand]');
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const deleteButton = document.querySelector('.delete');
+const allClearButton = document.querySelector('.all-clear');
+const previousOperand = document.querySelector('.previous-operand');
+const currentOperand = document.querySelector('.current-operand');
 
+//Add EventListeners
+numberButtons.forEach(button => {button.addEventListener('click', addNumberToOutput)});
+operationButtons.forEach(button => {button.addEventListener('click', addOperationToOutput)});
+//equalsButton.forEach(button => {button.addEventListener('click', calculateResult)});
+deleteButton.addEventListener('click', deleteFromOutput);
+allClearButton.addEventListener('click', clearOutput);
+
+
+//Functions
+function addNumberToOutput(){
+    if(this.innerText === '.' && currentOperand.innerText.includes('.')){
+        return;
+    } else{
+        currentOperand.innerText += this.innerText;
+    }}
+
+function addOperationToOutput(){
+    currentOperand.innerText += this.innerText;
+    previousOperand.innerText = currentOperand.innerText;
+    currentOperand.innerText = '';
+    }
+
+function deleteFromOutput(){
+    currentOperand.innerText = currentOperand.innerText.slice(0, -1);
+}
+
+function clearOutput(){
+    currentOperand.innerText = '';
+    previousOperand.innerText = '';
+}
+
+//function calculateResult(){
+//}
