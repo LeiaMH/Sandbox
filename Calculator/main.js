37904065374aaf258bd41f9fbd9b019f96dc4d57
Leia <leiahopf@googlemail.com>
2020-06-02 16:36:10 +0100

    Enhancement: Made calculateResult function.

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,3 +1,4 @@
+//Variables
 const numberButtons = document.querySelectorAll('.number');
 const operationButtons = document.querySelectorAll('.operation');
 const equalsButton = document.querySelector('.equals');
@@ -6,12 +7,12 @@ const allClearButton = document.querySelector('.all-clear');
 const previousOperand = document.querySelector('.previous-operand');
 const currentOperand = document.querySelector('.current-operand');
 
-//Add EventListeners
+//EventListeners
 numberButtons.forEach(button => {button.addEventListener('click', addNumberToOutput)});
 operationButtons.forEach(button => {button.addEventListener('click', addOperationToOutput)});
-//equalsButton.forEach(button => {button.addEventListener('click', calculateResult)});
 deleteButton.addEventListener('click', deleteFromOutput);
 allClearButton.addEventListener('click', clearOutput);
+equalsButton.addEventListener('click', calculateResult);
 
 
 //Functions
@@ -37,5 +38,27 @@ function clearOutput(){
     previousOperand.innerText = '';
 }
 
-//function calculateResult(){
-//}
+function calculateResult(){
+
+    let operator = previousOperand.innerText.slice(previousOperand.innerText.length - 1);
+    let firstNumber = parseFloat(previousOperand.innerText.slice(0, -1));
+    let secondNumber = parseFloat(currentOperand.innerText);
+
+   switch(operator){
+    case '÷':
+        result = firstNumber / secondNumber;
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        break;
+    case '+':
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        result = firstNumber - secondNumber;
+        break;
+   }
+
+   previousOperand.innerText = '';
+   currentOperand.innerText = result;
+}
